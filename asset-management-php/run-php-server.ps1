4b6bc9ba5863da125cad52a898c87c86b8836e3f
muteesasira tendo shamma <shammatendo1@gmail.com>
2026-05-14 20:55:12 +0300

    Delete asset-management-php directory
    not supposed to be in this branch

--- asset-management-php/run-php-server.ps1
+++
@@ -1,5 +0,0 @@
-# Saves database to: asset-management-php\data\assets.sqlite
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-Set-Location $here
-Write-Host "Open http://127.0.0.1:8080/ in your browser (PHP required)." -ForegroundColor Cyan
-php -S 127.0.0.1:8080 -t public public/router.php
